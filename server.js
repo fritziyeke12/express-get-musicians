cd8b4af7f9f7f760f5f49ddf00d69404a54270d4
fritziyeke12 <fritziyeke@gmail.com>
2023-02-20 15:51:52 +0000

    finito

--- server.js
+++ server.js
@@ -3,9 +3,13 @@ const app = express();
 const {Musician} = require("./Musician")
 const {sequelize} = require("./db")
 
-const port = 3000;
+const port = 3002;
 
 //TODO
+app.get("/musicians", async (req, res) => {
+    const arr = await Musician.findAll();
+    res.json(Array(arr[0]));
+})
 
 app.listen(port, () => {
     sequelize.sync();
