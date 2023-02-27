6832b682db3d36b5928366e8bb0c788ed8d114a4
fritziyeke12 <fritziyeke@gmail.com>
2023-02-27 11:24:54 +0000

    added middleware and CUD endpoints

--- server.js
+++ server.js
@@ -6,14 +6,39 @@ const {sequelize} = require("./db")
 const port = 3000;
 
 //TODO
+
+app.use(express.json());
+
 app.get("/musicians", async (req, res) => {
-    const arr = await Musician.findAll();
-    res.json(Array(arr[0]));
+    const all = await Musician.findAll();
+    res.json(all);
 })
 
 app.get('/musicians/:id', async (req, res) => {
-    const singer = await Musician.findByPk(req.params.id);
-    res.json(singer);
+    const mem = await Musician.findByPk(req.params.id);
+    res.json(mem);
+})
+
+app.post('/musicians', async (req, res) => {
+    await Musician.create(req.body);
+    const all = await Musician.findAll();
+    res.send(all);
+})
+
+app.put('/musicians/:id', async (req, res) => {
+    const mem = await Musician.findByPk(req.params.id);
+    await mem.update(req.body);
+    res.send(mem);
+})
+
+app.delete('/musicians/:id', async (req, res) => {
+    await Musician.destroy({
+        where: {
+            id: req.params.id
+        }
+    });
+    const all = await Musician.findAll();
+    res.send(all);
 })
 
 app.listen(port, () => {
